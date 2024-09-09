8487744dc72078fd7f515cd0edf120a520051936
Gabriel Lemos <gabrieldebritolemos@gmail.com>
2024-09-09 15:55:59 -0300

    feat: download history from google sheets

--- src/stock/stock.service.ts
+++ src/stock/stock.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, Inject } from '@nestjs/common';
+import { isEqual } from 'lodash';
+import { format } from 'date-fns';
 
 import type { Sheets } from './sheets.factory';
 import { GOOGLE_SHEETS, SPREADSHEET_ID } from './stock.constants';
@@ -10,13 +12,58 @@ export class StockService {
     @Inject(SPREADSHEET_ID) private spreadsheetId: string,
   ) {}
 
-  async getStock(ticket: string): Promise<string> {
+  formatSheetDate(date?: string): string {
+    const SHEET_DATE_FORMAT = "'DATE('yyyy, MM, dd')'";
+    return format(date ? new Date(date) : new Date(), SHEET_DATE_FORMAT);
+  }
+
+  handleHistoryData([date, open, high, low, close, volume]: string[]) {
+    return {
+      date: new Date(date),
+      open: parseFloat(open),
+      high: parseFloat(high),
+      low: parseFloat(low),
+      close: parseFloat(close),
+      volume: parseInt(volume, 10),
+    };
+  }
+
+  async downloadStockHistory(ticket: string) {
+    const start = this.formatSheetDate('2024-1-1');
+    const today = this.formatSheetDate();
+
+    await this.sheets.spreadsheets.values.update({
+      spreadsheetId: this.spreadsheetId,
+      range: 'Sheet1!A1',
+      valueInputOption: 'USER_ENTERED',
+      requestBody: {
+        values: [
+          [ticket, `=${start}`, `=${today}`],
+          [`=GOOGLEFINANCE("${ticket}", "all", ${start}, ${today})`],
+        ],
+      },
+    });
+
     const res = await this.sheets.spreadsheets.values.get({
       spreadsheetId: this.spreadsheetId,
       range: 'Sheet1',
     });
 
-    console.log('==== res', res.data.values);
+    const [queryData, headers, ...history] = res.data.values;
+    const expectedHeaders = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume'];
+
+    if (!queryData || queryData[0] !== ticket) {
+      throw new Error('Data conflict: ticket mismatch');
+    } else if (!isEqual(headers, expectedHeaders)) {
+      throw new Error('Data conflict: header mismatch');
+    }
+
+    // handle history and store locally
+    (history as string[][]).map(this.handleHistoryData).forEach(console.log);
+  }
+
+  async getStock(ticket: string): Promise<string> {
+    await this.downloadStockHistory(ticket);
     return `This action returns all coffees for ticket ${ticket}`;
   }
 }
