d160a93659e10f5f02a1ebf85f5f4b12922f6438
Gabriel Lemos <gabrieldebritolemos@gmail.com>
2024-11-01 01:01:37 -0300

    feat: added AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { LogModule } from './core/log/log.module';
 import { CommonModule } from './common/common.module';
 import { ConfigureModule } from './core/configure/configure.module';
+import { AuthModule } from './core/auth/auth.module';
 import { SpreadsheetsModule } from './project/spreadsheets/spreadsheets.module';
 import { StockModule } from './project/stock/stock.module';
 
@@ -11,6 +12,7 @@ import { StockModule } from './project/stock/stock.module';
     LogModule,
     CommonModule,
     ConfigureModule,
+    AuthModule,
     SpreadsheetsModule,
     StockModule,
   ],
