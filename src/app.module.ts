abc77407ccf0919acd7de9387bef7272aea0279f
Gabriel Lemos <gabrieldebritolemos@gmail.com>
2024-11-01 01:04:12 -0300

    refactor: omitted modules that are imported by others

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { LogModule } from './core/log/log.module';
 import { CommonModule } from './common/common.module';
 import { ConfigureModule } from './core/configure/configure.module';
 import { AuthModule } from './core/auth/auth.module';
-import { SpreadsheetsModule } from './project/spreadsheets/spreadsheets.module';
 import { StockModule } from './project/stock/stock.module';
 
 @Module({
-  imports: [
-    LogModule,
-    CommonModule,
-    ConfigureModule,
-    AuthModule,
-    SpreadsheetsModule,
-    StockModule,
-  ],
+  imports: [CommonModule, ConfigureModule, AuthModule, StockModule],
 })
 export class AppModule {}
